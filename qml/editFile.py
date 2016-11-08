41b83b9bbbf96d21927731dd06843ba9bc233c17
eekkelund <eetu.kahelin@metropolia.fi>
2016-11-08 23:36:28 +0200

    Added autosave

--- qml/editFile.py
+++ qml/editFile.py
@@ -8,15 +8,24 @@ def openings(filepath):
     if os.path.exists(filepath):
         file = open(filepath, 'r')
     else:
-        file = open(filepath, 'w')
+        file = open(filepath, 'a+')
     txt = file.read()
     file.close()
     return txt
 
-
-
 def savings(filepath, text):
+    if os.path.exists(filepath+"~"):
+        os.remove(filepath+"~")
     file = open(filepath, 'w')
+    file.write(text)
+    file.close()
+    return
+
+def autosave(filepath, text):
+    if(filepath.endswith("~")):
+        file = open(filepath, 'w')
+    else:
+        file = open(filepath+"~", 'w')
     file.write(text)
     file.close()
     return
