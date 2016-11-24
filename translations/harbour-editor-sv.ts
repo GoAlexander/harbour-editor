61fc3aa00de75273255d4551636d04d8bf917544
Åke Engelbrektson <eson57@users.noreply.github.com>
2016-11-24 08:21:43 +0100

    Create harbour-editor-sv.ts

---
+++ translations/harbour-editor-sv.ts
@@ -0,0 +1,156 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="filemanager-la-file_details">
+        <source>%1, %2</source>
+        <extracomment>Shows size and modification date, e.g. &quot;15.5MB, 02/03/2016&quot;</extracomment>
+        <translation>%1, %2</translation>
+    </message>
+    <message id="filemanager-la-no_files">
+        <source>No files</source>
+        <translation>Inga filer</translation>
+    </message>
+</context>
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>&quot;Editor.&quot; is feature-rich text/code editor!</source>
+        <translation>&quot;Editor.&quot; är en funktionsrik text-/kodredigerare!</translation>
+    </message>
+    <message>
+        <source>License: GPLv3</source>
+        <translation>Licens: GPLv3</translation>
+    </message>
+    <message>
+        <source>You can find the source code at the:</source>
+        <translation>Du hittar källkoden på:</translation>
+    </message>
+    <message>
+        <source>GitHub</source>
+        <translation>GitHub</translation>
+    </message>
+    <message>
+        <source>Special thanks:</source>
+        <translation>Tack till:</translation>
+    </message>
+    <message>
+        <source>Tips:</source>
+        <translation>Tips:</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>-coderus for various tips and code</source>
+        <translation>-coderus, för diverse tips och kod</translation>
+    </message>
+    <message>
+        <source>-To &apos;Select all text&apos; hold your finger until 3 vibrations</source>
+        <translation>-För &quot;Markera all text&quot;, långtryck till 3 vibrationer</translation>
+    </message>
+    <message>
+        <source>-osanwe for very often consultations about qml code</source>
+        <translation>-osanwe, för återkommande konsultationer om QML-kod</translation>
+    </message>
+    <message>
+        <source>or</source>
+        <translation>eller</translation>
+    </message>
+    <message>
+        <source>Star the repository at the github ☺</source>
+        <translation>Stjärnmärka kodförrådet på GitHub ☺</translation>
+    </message>
+    <message>
+        <source>If you want to support the developer:</source>
+        <translation>Om du vill stödja utvecklaren:</translation>
+    </message>
+    <message>
+        <source>-eekkelund for save/load/autosave functions and feedback</source>
+        <translation>-eekkelund, för spar-/inläsnings-/autosparfunktioner och återkoppling</translation>
+    </message>
+    <message>
+        <source>-Unsaved changes are saved in the file ending with &apos;~&apos; in the same dir where you placed your original file</source>
+        <translation>-Osparade ändringar sparas i filen som slutar med tilde (~), i samma mapp som där du placerat originalfilen</translation>
+    </message>
+    <message>
+        <source>Paypal donation</source>
+        <translation>Paypal-donation</translation>
+    </message>
+    <message>
+        <source>Make a donation (button above)</source>
+        <translation>Donera (ovanstående knapp)</translation>
+    </message>
+    <message>
+        <source>-Russian community for feedback and help</source>
+        <translation>-Ryska gemenskapen, för åtekoppling och hjälp</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Lines: </source>
+        <translation>Rader: </translation>
+    </message>
+    <message>
+        <source>Chars: </source>
+        <translation>Tecken: </translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <source>R-only</source>
+        <translation>R-only</translation>
+    </message>
+    <message>
+        <source>Save as</source>
+        <translation>Spara som</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Öppna</translation>
+    </message>
+    <message>
+        <source>Save   </source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <source>Document saved!</source>
+        <translation>Dokument sparat!</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation>Utseende</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Quick menu enabled</source>
+        <translation>Snabbmeny aktiverad</translation>
+    </message>
+    <message>
+        <source>Line numeration enabled</source>
+        <translation>Radnumrering aktiverad</translation>
+    </message>
+</context>
+</TS>
