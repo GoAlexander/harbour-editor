e9e21a8fda0be3862b64b9e8a65fc603abc17e42
GoAlexander <AlexanderDydychkin@yandex.ru>
2016-12-10 14:40:31 +0300

    Added russian translation

---
+++ translations/harbour-editor-ru.ts
@@ -0,0 +1,196 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="filemanager-la-file_details">
+        <source>%1, %2</source>
+        <extracomment>Shows size and modification date, e.g. &quot;15.5MB, 02/03/2016&quot;</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="filemanager-la-no_files">
+        <source>No files</source>
+        <translation>Файлы отсутствуют</translation>
+    </message>
+</context>
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>&quot;Editor.&quot; is feature-rich text/code editor!</source>
+        <translation>&quot;Editor.&quot; является многофункциональным текстовым редактором!</translation>
+    </message>
+    <message>
+        <source>License: GPLv3</source>
+        <translation>Лицензия: GPLv3</translation>
+    </message>
+    <message>
+        <source>You can find the source code at the:</source>
+        <translation>Исходный код можно найти:</translation>
+    </message>
+    <message>
+        <source>GitHub</source>
+        <translation>GitHub</translation>
+    </message>
+    <message>
+        <source>Special thanks:</source>
+        <translation>Особенная благодарность:</translation>
+    </message>
+    <message>
+        <source>Tips:</source>
+        <translation>Советы:</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>-coderus for various tips and code</source>
+        <translation>-coderus за различные советы и код</translation>
+    </message>
+    <message>
+        <source>-To &apos;Select all text&apos; hold your finger until 3 vibrations</source>
+        <translation>-Чтобы &apos;Выбрать весь текст&apos; удерживайте Ваш палец до 3-ёх вибраций</translation>
+    </message>
+    <message>
+        <source>-osanwe for very often consultations about qml code</source>
+        <translation>-osanwe за консультации насчет qml кода</translation>
+    </message>
+    <message>
+        <source>or</source>
+        <translation>или</translation>
+    </message>
+    <message>
+        <source>If you want to support the developer:</source>
+        <translation>Если Вы хотите поддержать разработчика:</translation>
+    </message>
+    <message>
+        <source>-eekkelund for save/load/autosave functions and feedback</source>
+        <translation>-eekkelund за функции сохранения/загрузки/автосохранения, а также обратную связь</translation>
+    </message>
+    <message>
+        <source>Paypal donation</source>
+        <translation>Paypal пожертвование</translation>
+    </message>
+    <message>
+        <source>Make a donation (button above)</source>
+        <translation>Сделайте пожертвование (кнопка выше)</translation>
+    </message>
+    <message>
+        <source>-Russian community for feedback and help</source>
+        <translation>-русскоговорящее сообщество за обратную связь и помощь</translation>
+    </message>
+    <message>
+        <source>-Unsaved changes are saved in the file with ending &apos;~&apos; in the same dir where you placed your original file</source>
+        <translation>-Несохраненные изменения сохраняются в файле, заканчивающимся на &apos;~&apos;, расположенный в той же папке, что и оригинальный файл</translation>
+    </message>
+    <message>
+        <source>Star the repository at the github ☺ </source>
+        <translation>Голосуйте за репозиторий в Github ☺ </translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Lines: </source>
+        <translation>Строк:</translation>
+    </message>
+    <message>
+        <source>Chars: </source>
+        <translation>Символов:</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Сохр.</translation>
+    </message>
+    <message>
+        <source>R-only</source>
+        <translation>Т-чтен.</translation>
+    </message>
+    <message>
+        <source>Save as</source>
+        <translation>Сохр. как</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <source>Save   </source>
+        <translation>Сохр.</translation>
+    </message>
+    <message>
+        <source>Document saved!</source>
+        <translation>Документ сохранен!</translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translation>Новый</translation>
+    </message>
+    <message>
+        <source>Document can`t be saved!</source>
+        <translation> Документ не может быть сохранен!</translation>
+    </message>
+    <message>
+        <source>Tab</source>
+        <translation>Таб</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation>Внешний вид</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>Quick menu enabled</source>
+        <translation>Быстрое меню включено</translation>
+    </message>
+    <message>
+        <source>Line numeration enabled</source>
+        <translation>Нумерация строк включена</translation>
+    </message>
+    <message>
+        <source>Fonts and size</source>
+        <translation>Шрифт и размер</translation>
+    </message>
+    <message>
+        <source>Use it to get more space for text</source>
+        <translation>Используйте, чтобы получить больше места для текста</translation>
+    </message>
+    <message>
+        <source>Font size:</source>
+        <translation>Размер шрифта:</translation>
+    </message>
+    <message>
+        <source>Font:</source>
+        <translation>Шрифт:</translation>
+    </message>
+    <message>
+        <source>File browser</source>
+        <translation>Файловый браузер</translation>
+    </message>
+    <message>
+        <source>Show hidden files</source>
+        <translation>Показывать скрытые файлы</translation>
+    </message>
+    <message>
+        <source>Be careful to enable this option!</source>
+        <translation>Будьте осторожны, активируя эту функцию!</translation>
+    </message>
+</context>
+</TS>
