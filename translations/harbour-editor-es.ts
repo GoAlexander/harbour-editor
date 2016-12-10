7fa7117442efc2daa9015324d057bbcd96110a92
GoAlexander <AlexanderDydychkin@yandex.ru>
2016-12-10 13:52:46 +0300

    Added *-es.ts file

---
+++ translations/harbour-editor-es.ts
@@ -0,0 +1,196 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="filemanager-la-file_details">
+        <source>%1, %2</source>
+        <extracomment>Shows size and modification date, e.g. &quot;15.5MB, 02/03/2016&quot;</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="filemanager-la-no_files">
+        <source>No files</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>&quot;Editor.&quot; is feature-rich text/code editor!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>License: GPLv3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You can find the source code at the:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>GitHub</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Special thanks:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tips:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>-coderus for various tips and code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>-To &apos;Select all text&apos; hold your finger until 3 vibrations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>-osanwe for very often consultations about qml code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>or</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If you want to support the developer:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>-eekkelund for save/load/autosave functions and feedback</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Paypal donation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Make a donation (button above)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>-Russian community for feedback and help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>-Unsaved changes are saved in the file with ending &apos;~&apos; in the same dir where you placed your original file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Star the repository at the github ☺ </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Lines: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Chars: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>R-only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save as</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save   </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Document saved!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Document can`t be saved!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Quick menu enabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Line numeration enabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fonts and size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Use it to get more space for text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Font size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Font:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show hidden files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Be careful to enable this option!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
