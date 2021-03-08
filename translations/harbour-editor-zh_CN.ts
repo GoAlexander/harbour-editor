10dd545e9e58e0b2ce22e78799538a3f7ac557fd
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2021-03-09 01:00:27 +0800

    Create harbour-editor-zh_CN.ts

---
+++ translations/harbour-editor-zh_CN.ts
@@ -0,0 +1,431 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name></name>
+    <message id="filemanager-la-file_details">
+        <source>%1, %2</source>
+        <extracomment>Shows size and modification date, e.g. &quot;15.5MB, 02/03/2016&quot;</extracomment>
+        <translation>%1, %2</translation>
+    </message>
+    <message id="filemanager-la-no_files">
+        <source>No files</source>
+        <translation>无文件</translation>
+    </message>
+</context>
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>&quot;Editor.&quot; is feature-rich text/code editor!</source>
+        <translation>&quot;Editor.&quot; 是一个功能丰富的文本及代码编辑器！<</translation>
+    </message>
+    <message>
+        <source>License: GPLv3</source>
+        <translation>许可证: GPLv3</translation>
+    </message>
+    <message>
+        <source>Special thanks:</source>
+        <translation>特别感谢:</translation>
+    </message>
+    <message>
+        <source>Tips:</source>
+        <translation>建议:</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>-coderus for various tips and code</source>
+        <translation>-coderus 提供各种建议及代码</translation>
+    </message>
+    <message>
+        <source>-To &apos;Select all text&apos; hold your finger until 3 vibrations</source>
+        <translation>如果想要 &apos;全选 text&apos; 按住手指直到3次振动</translation>
+    </message>
+    <message>
+        <source>-osanwe for very often consultations about qml code</source>
+        <translation>-osanwe 经常提供 qml 代码方面的咨询</translation>
+    </message>
+    <message>
+        <source>or</source>
+        <translation>或</translation>
+    </message>
+    <message>
+        <source>If you want to support the developer:</source>
+        <translation>如果你想要支持开发者:</translation>
+    </message>
+    <message>
+        <source>Make a donation (button above)</source>
+        <translation>捐赠(上方按钮)</translation>
+    </message>
+    <message>
+        <source>-Russian community for feedback and help</source>
+        <translation>-俄罗斯社区提供反馈及帮助</translation>
+    </message>
+    <message>
+        <source>-Unsaved changes are saved in the file with ending &apos;~&apos; in the same dir where you placed your original file</source>
+        <translation>未保存的修改被保存于以下述结尾的文件 &apos;~&apos; 和原文件保存于相同目录。</translation>
+    </message>
+    <message>
+        <source>Star the repository at the github ☺ </source>
+        <translation>在 GitHub Star(标星)☺ </translation>
+    </message>
+    <message>
+        <source>Translators</source>
+        <translation>翻译者</translation>
+    </message>
+    <message>
+        <source> - Spanish translation</source>
+        <translation> - 西班牙语翻译</translation>
+    </message>
+    <message>
+        <source> - Swedish translation</source>
+        <translation> - 瑞典语翻译</translation>
+    </message>
+    <message>
+        <source> - Russian translation</source>
+        <translation> - 俄语翻译</translation>
+    </message>
+    <message>
+        <source> - French translation</source>
+        <translation> - 法语翻译</translation>
+    </message>
+    <message>
+        <source> - Polish translation</source>
+        <translation> - 波兰语翻译</translation>
+    </message>
+    <message>
+        <source>-You can copy the file path to the clipboard by selecting appropriate MenuItem in pulley menu</source>
+        <translation>-你可以通过滑轮菜单项目复制文件目录到剪切板</translation>
+    </message>
+    <message>
+        <source>-Ancelad for tab icon, testing and help</source>
+        <translation>Ancelad 提供按钮图标、测试及帮助</translation>
+    </message>
+    <message>
+        <source> - German translation</source>
+        <translation> - 德语翻译</translation>
+    </message>
+    <message>
+        <source> - Dutch translation</source>
+        <translation> - 尼德兰语翻译</translation>
+    </message>
+    <message>
+        <source>-gri4994 for the wonderful app icon</source>
+        <translation>-gri4994 提供精美的软件图标</translation>
+    </message>
+    <message>
+        <source>-eekkelund for save/load/autosave functions, source code and some dictionaries for syntax highlighting and feedback</source>
+        <translation>eekelund 提供保存、加载、自动保存功能、代码及某些 syntax 字典强调功能及反馈。</translation>
+    </message>
+    <message>
+        <source>-the team of GtkSourceView for the basis for .sh dictionary</source>
+        <translation>GtkSourceView 团队提供 .sh 字典基础</translation>
+    </message>
+    <message>
+        <source>Source code</source>
+        <translation>源代码</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>版本</translation>
+    </message>
+    <message>
+        <source>Paypal donation - RUB</source>
+        <translation>Paypal 捐赠 - 卢布</translation>
+    </message>
+    <message>
+        <source>Paypal donation - EURO</source>
+        <translation>Paypal-捐赠 - 欧元</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Lines: </source>
+        <translation>行数: </translation>
+    </message>
+    <message>
+        <source>Words: </source>
+        <translation>词语: </translation>
+    </message>
+    <message>
+        <source>Chars: </source>
+        <translation>字符: </translation>
+    </message>
+</context>
+<context>
+    <name>EditRow</name>
+    <message>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <source>Document can&apos;t be saved!</source>
+        <translation>文档无法保存！</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>重写</translation>
+    </message>
+    <message>
+        <source>Tab</source>
+        <translation>标签</translation>
+    </message>
+    <message>
+        <source>Document saved</source>
+        <translation>文档已保存</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>搜索</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>File path copied to the clipboard</source>
+        <translation>文件路径已复制到剪切板</translation>
+    </message>
+    <message>
+        <source>Document saved!</source>
+        <translation>文档已保存！</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <source>Error while opening/saving the file</source>
+        <translation>打开或保存文件时出错</translation>
+    </message>
+    <message>
+        <source>Highlighting enabled</source>
+        <translation>强调已启用</translation>
+    </message>
+    <message>
+        <source>Highlighting disabled</source>
+        <translation>强调已禁用</translation>
+    </message>
+</context>
+<context>
+    <name>HistoryPage</name>
+    <message>
+        <source>History</source>
+        <translation>历史</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <source>Move to top</source>
+        <translation>移动到顶部</translation>
+    </message>
+    <message>
+        <source>Documents are in order of first opening</source>
+        <translation>文档按第一次打开的顺序打开</translation>
+    </message>
+    <message>
+        <source>Delete history</source>
+        <translation>删除历史</translation>
+    </message>
+</context>
+<context>
+    <name>MainRow</name>
+    <message>
+        <source>Save as</source>
+        <translation>保存为</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>打开</translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translation>新建</translation>
+    </message>
+    <message>
+        <source>History</source>
+        <translation>历史</translation>
+    </message>
+    <message>
+        <source>R-only</source>
+        <translation>R-only</translation>
+    </message>
+    <message>
+        <source>Quick note</source>
+        <translation>快速笔记</translation>
+    </message>
+    <message>
+        <source>Highlight</source>
+        <translation>强调</translation>
+    </message>
+</context>
+<context>
+    <name>QuickNotePage</name>
+    <message>
+        <source>Undo</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>重写</translation>
+    </message>
+    <message>
+        <source>Tab</source>
+        <translation>标签</translation>
+    </message>
+    <message>
+        <source>Text auto-saved in:</source>
+        <translation>文本自动保存于:</translation>
+    </message>
+    <message>
+        <source>Quick note.</source>
+        <translation>快速笔记。</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>搜索</translation>
+    </message>
+</context>
+<context>
+    <name>SearchRow</name>
+    <message>
+        <source>Search</source>
+        <translation>搜索</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation>外观</translation>
+    </message>
+    <message>
+        <source>Line numeration enabled</source>
+        <translation>行数计算已启用</translation>
+    </message>
+    <message>
+        <source>Line numeration disabled</source>
+        <translation>行数计算已禁用</translation>
+    </message>
+    <message>
+        <source>Dark Theme</source>
+        <translation>深色主题</translation>
+    </message>
+    <message>
+        <source>Ambience Theme</source>
+        <translation>氛围主题</translation>
+    </message>
+    <message>
+        <source>Custom button color</source>
+        <translation>自定义按钮颜色</translation>
+    </message>
+    <message>
+        <source>Fonts and size</source>
+        <translation>字体及大小</translation>
+    </message>
+    <message>
+        <source>Font size</source>
+        <translation>字体大小</translation>
+    </message>
+    <message>
+        <source>Tiny</source>
+        <translation>微小号</translation>
+    </message>
+    <message>
+        <source>Extra small</source>
+        <translation>特小号</translation>
+    </message>
+    <message>
+        <source>Small</source>
+        <translation>小号</translation>
+    </message>
+    <message>
+        <source>Medium (default)</source>
+        <translation>中号 (默认)</translation>
+    </message>
+    <message>
+        <source>Large</source>
+        <translation>大号</translation>
+    </message>
+    <message>
+        <source>Extra large</source>
+        <translation>超大号</translation>
+    </message>
+    <message>
+        <source>Huge</source>
+        <translation>巨大号</translation>
+    </message>
+    <message>
+        <source>Font:</source>
+        <translation>字体:</translation>
+    </message>
+    <message>
+        <source>Type of tab:</source>
+        <translation>标签类型:</translation>
+    </message>
+    <message>
+        <source>Default text editor</source>
+        <translation>默认文本编辑器</translation>
+    </message>
+    <message>
+        <source>Turn off to enable default notes</source>
+        <translation>关闭以启用默认备忘录软件</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>常规</translation>
+    </message>
+    <message>
+        <source>Autosave</source>
+        <translation>自动保存</translation>
+    </message>
+    <message>
+        <source>If enabled text will be stored in files with postfix ~</source>
+        <translation>如果启用，文本会被保存到带有~结尾的文件之中</translation>
+    </message>
+    <message>
+        <source>File browser</source>
+        <translation>文件管理器</translation>
+    </message>
+    <message>
+        <source>Show hidden files</source>
+        <translation>显示隐藏文件</translation>
+    </message>
+    <message>
+        <source>Be careful to enable this option!</source>
+        <translation>请谨慎启用此选项！</translation>
+    </message>
+    <message>
+        <source>Encoding</source>
+        <translation>编码</translation>
+    </message>
+    <message>
+        <source>Region:</source>
+        <translation>区域:</translation>
+    </message>
+    <message>
+        <source>Reset settings</source>
+        <translation>重置设置</translation>
+    </message>
+    <message>
+        <source>Save settings</source>
+        <translation>保存设置</translation>
+    </message>
+</context>
+</TS>
