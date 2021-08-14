84d6edb2c7d7834d3e834f2bd8f83931f7178c2d
okruhliak <ladislav@hodas.sk>
2021-08-14 17:36:05 +0200

    Update harbour-editor-sk.ts

---
+++ translations/harbour-editor-sk.ts
@@ -0,0 +1,435 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="filemanager-la-file_details">
+        <source>%1, %2</source>
+        <extracomment>Shows size and modification date, e.g. &quot;15.5MB, 02/03/2016&quot;</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="filemanager-la-no_files">
+        <source>No files</source>
+        <translation>Žiadne súbory</translation>
+    </message>
+</context>
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>&quot;Editor.&quot; is feature-rich text/code editor!</source>
+        <translation>&quot;Editor.&quot; je rozšírený editor textu/programu!</translation>
+    </message>
+    <message>
+        <source>License: GPLv3</source>
+        <translation>Licencia: GPLv3</translation>
+    </message>
+    <message>
+        <source>Special thanks:</source>
+        <translation>Zvláštne poďakovanie:</translation>
+    </message>
+    <message>
+        <source>Tips:</source>
+        <translation>Tipy:</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>О aplikácii</translation>
+    </message>
+    <message>
+        <source>-coderus for various tips and code</source>
+        <translation>-coderus za rôzne rady a programovanie</translation>
+    </message>
+    <message>
+        <source>-To &apos;Select all text&apos; hold your finger until 3 vibrations</source>
+        <translation>-Pre &apos;Označiť celý text&apos; treba podržať prst pokiaľ 3-krát zavibruje</translation>
+    </message>
+    <message>
+        <source>-osanwe for very often consultations about qml code</source>
+        <translation>-osanwe za veľmi časté konzultácie ohľadom kódu qml</translation>
+    </message>
+    <message>
+        <source>or</source>
+        <translation>alebo</translation>
+    </message>
+    <message>
+        <source>If you want to support the developer:</source>
+        <translation>Ak chcete podporiť vývojára:</translation>
+    </message>
+    <message>
+        <source>Make a donation (button above)</source>
+        <translation>Darovať (tlačidlo vyššie)</translation>
+    </message>
+    <message>
+        <source>-Russian community for feedback and help</source>
+        <translation>-Ruskej komunite za názory a pomoc</translation>
+    </message>
+    <message>
+        <source>-Unsaved changes are saved in the file with ending &apos;~&apos; in the same dir where you placed your original file</source>
+        <translation>-Neuložené zmeny sa nachádzajú v súbore s koncovkou &apos;~&apos;, v tej istej zložke, kde ste uložili originálny súbor</translation>
+    </message>
+    <message>
+        <source>Star the repository at the github ☺ </source>
+        <translation>Ohodnotiť hviezdičkou repozitár na Github ☺ </translation>
+    </message>
+    <message>
+        <source>Translators</source>
+        <translation>Prekladatelia</translation>
+    </message>
+    <message>
+        <source> - Spanish translation</source>
+        <translation> - Preklad do španielčiny</translation>
+    </message>
+    <message>
+        <source> - Swedish translation</source>
+        <translation> - Preklad do švédčiny</translation>
+    </message>
+    <message>
+        <source> - Russian translation</source>
+        <translation> - Preklad do ruštiny</translation>
+    </message>
+    <message>
+        <source> - French translation</source>
+        <translation> - Preklad do francúžštiny</translation>
+    </message>
+    <message>
+        <source>-Ancelad for tab icon, testing and help</source>
+        <translation>-Ancelad na ikone karty, testovanie a pomoc</translation>
+    </message>
+    <message>
+        <source>-You can copy the file path to the clipboard by selecting appropriate MenuItem in pulley menu</source>
+        <translation>-Cestu k súboru môžete skopírovať do schránky výberom príslušnej položky v ponuke sťahovacieho menu</translation>
+    </message>
+    <message>
+        <source> - Polish translation</source>
+        <translation> - Preklad do poľštiny</translation>
+    </message>
+    <message>
+        <source> - German translation</source>
+        <translation> - Preklad do nemčiny</translation>
+    </message>
+    <message>
+        <source> - Dutch translation</source>
+        <translation> - Preklad do holandčiny</translation>
+    </message>
+    <message>
+        <source>-gri4994 for the wonderful app icon</source>
+        <translation>-gri4994 za nádhernú ikonu aplikácie</translation>
+    </message>
+    <message>
+        <source>-eekkelund for save/load/autosave functions, source code and some dictionaries for syntax highlighting and feedback</source>
+        <translation>-eekkelund za funkcie uložiť/načítať/samočinne uložiť, zdrojový text a niekoľko adresárov pre úpravu syntaxe a názory</translation>
+    </message>
+    <message>
+        <source>-the team of GtkSourceView for the basis for .sh dictionary</source>
+        <translation>-tímu GtkSourceView za základy pre .sh adresár</translation>
+    </message>
+    <message>
+        <source>Source code</source>
+        <translation>Zdrojový text</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Verzia</translation>
+    </message>
+    <message>
+        <source>Paypal donation - RUB</source>
+        <translation>Darovanie Paypal - RUB</translation>
+    </message>
+    <message>
+        <source>Paypal donation - EURO</source>
+        <translation>Darovanie Paypal - EURO</translation>
+    </message>
+    <message>
+        <source>-bobsik for the contributions for version 0.9</source>
+        <translation>-bobsik za príspevky k verzii 0.9</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Lines: </source>
+        <translation>Riadky: </translation>
+    </message>
+    <message>
+        <source>Words: </source>
+        <translation>Slová: </translation>
+    </message>
+    <message>
+        <source>Chars: </source>
+        <translation>Znaky: </translation>
+    </message>
+</context>
+<context>
+    <name>EditRow</name>
+    <message>
+        <source>Save</source>
+        <translation>Uložiť</translation>
+    </message>
+    <message>
+        <source>Document can&apos;t be saved!</source>
+        <translation>Dokument nebolo možné uložiť!</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>Späť</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>Vpred</translation>
+    </message>
+    <message>
+        <source>Tab</source>
+        <translation>Tab</translation>
+    </message>
+    <message>
+        <source>Document saved</source>
+        <translation>Dokument bol uložený</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Hľadať</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>File path copied to the clipboard</source>
+        <translation>Cesta k súboru bola skopírovaná do schránky</translation>
+    </message>
+    <message>
+        <source>Document saved!</source>
+        <translation>Dokument bol uložený!</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Nastavenia</translation>
+    </message>
+    <message>
+        <source>Error while opening/saving the file</source>
+        <translation>Nastala chyba počas otvárania/ukladania súboru</translation>
+    </message>
+    <message>
+        <source>Highlighting enabled</source>
+        <translation>Zvýrazňovanie povolené</translation>
+    </message>
+    <message>
+        <source>Highlighting disabled</source>
+        <translation>Zvýrazňovanie zakázané</translation>
+    </message>
+</context>
+<context>
+    <name>HistoryPage</name>
+    <message>
+        <source>History</source>
+        <translation>História</translation>
+    </message>
+    <message>
+        <source>Move to top</source>
+        <translation>Presunúť hore</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Odstrániť</translation>
+    </message>
+    <message>
+        <source>Documents are in order of first opening</source>
+        <translation>Dokumenty sú zoradené podľa otvorenia chronologicky</translation>
+    </message>
+    <message>
+        <source>Delete history</source>
+        <translation>Odstrániť históriu</translation>
+    </message>
+</context>
+<context>
+    <name>MainRow</name>
+    <message>
+        <source>Save as</source>
+        <translation>Ul. ako</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Otvoriť</translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translation>Nový</translation>
+    </message>
+    <message>
+        <source>History</source>
+        <translation>Hist.</translation>
+    </message>
+    <message>
+        <source>R-only</source>
+        <translation>Iba čít</translation>
+    </message>
+    <message>
+        <source>Quick note</source>
+        <translation>Stručná poznámka</translation>
+    </message>
+    <message>
+        <source>Highlight</source>
+        <translation>Zvýrazniť</translation>
+    </message>
+</context>
+<context>
+    <name>QuickNotePage</name>
+    <message>
+        <source>Undo</source>
+        <translation>Späť</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>Vpred</translation>
+    </message>
+    <message>
+        <source>Tab</source>
+        <translation>Tab</translation>
+    </message>
+    <message>
+        <source>Text auto-saved in:</source>
+        <translation>Text samočinne uložený do:</translation>
+    </message>
+    <message>
+        <source>Quick note.</source>
+        <translation>Stručná poznámka.</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Hľadať</translation>
+    </message>
+</context>
+<context>
+    <name>SearchRow</name>
+    <message>
+        <source>Search</source>
+        <translation>Hľadať</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>About</source>
+        <translation>О aplikácii</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Nastavenia</translation>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation>Vzhľad</translation>
+    </message>
+    <message>
+        <source>Line numeration enabled</source>
+        <translation>Číslovanie riadkov povolené</translation>
+    </message>
+    <message>
+        <source>Line numeration disabled</source>
+        <translation>Číslovanie riadkov zakázané</translation>
+    </message>
+    <message>
+        <source>Dark Theme</source>
+        <translation>Tmavá téma</translation>
+    </message>
+    <message>
+        <source>Ambience Theme</source>
+        <translation>Téma prostredia</translation>
+    </message>
+    <message>
+        <source>Custom button color</source>
+        <translation>Vlastná farba tlačidiel</translation>
+    </message>
+    <message>
+        <source>Fonts and size</source>
+        <translation>Písma a veľkosti</translation>
+    </message>
+    <message>
+        <source>Font size</source>
+        <translation>Veľkosť písma</translation>
+    </message>
+    <message>
+        <source>Tiny</source>
+        <translation>Maličké</translation>
+    </message>
+    <message>
+        <source>Extra small</source>
+        <translation>Veľmi malé</translation>
+    </message>
+    <message>
+        <source>Small</source>
+        <translation>Malé</translation>
+    </message>
+    <message>
+        <source>Medium (default)</source>
+        <translation>Stredné (predvolené)</translation>
+    </message>
+    <message>
+        <source>Large</source>
+        <translation>Veľké</translation>
+    </message>
+    <message>
+        <source>Extra large</source>
+        <translation>Veľmi veľké</translation>
+    </message>
+    <message>
+        <source>Huge</source>
+        <translation>Obrovské</translation>
+    </message>
+    <message>
+        <source>Font:</source>
+        <translation>Písmo:</translation>
+    </message>
+    <message>
+        <source>Type of tab:</source>
+        <translation>Druh tabulátora</translation>
+    </message>
+    <message>
+        <source>Default text editor</source>
+        <translation>Predvolený textový editor</translation>
+    </message>
+    <message>
+        <source>Turn off to enable default notes</source>
+        <translation>Vypnúť pre povolenie predvolených poznámok</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Všedobecný</translation>
+    </message>
+    <message>
+        <source>Autosave</source>
+        <translation>Samočinne ukladať</translation>
+    </message>
+    <message>
+        <source>If enabled text will be stored in files with postfix ~</source>
+        <translation>Ak je povolené, text bude ukladaný do súborov s príponou ~</translation>
+    </message>
+    <message>
+        <source>File browser</source>
+        <translation>Prehliadač súborov</translation>
+    </message>
+    <message>
+        <source>Show hidden files</source>
+        <translation>Zobraziť skryté súbory</translation>
+    </message>
+    <message>
+        <source>Be careful to enable this option!</source>
+        <translation>Buďte opatrný pri povoľovaní tejto možnosti!</translation>
+    </message>
+    <message>
+        <source>Encoding</source>
+        <translation>Kódovanie</translation>
+    </message>
+    <message>
+        <source>Region:</source>
+        <translation>Oblasť:</translation>
+    </message>
+    <message>
+        <source>Reset settings</source>
+        <translation>Obnoviť nastavenia</translation>
+    </message>
+    <message>
+        <source>Save settings</source>
+        <translation>Uložiť nastavenia</translation>
+    </message>
+</context>
+</TS>
